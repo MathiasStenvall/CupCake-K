4879401731326133589aaed6a04863fba351b30e
ChristianKenter <cph-cn445@stud.ek.dk>
2026-04-08 17:22:51 +0200

    1). Created a ClientController class (And added an addRoutes-method).
    2). Created a AdminController class (And added an addRoutes-method).
    
    3). Updated and created new packages, classes and methods in UML-diagram, so it matches java-code 1:1.

---
+++ src/main/java/controllers/AdminController.java
@@ -0,0 +1,11 @@
+package controllers;
+
+import io.javalin.Javalin;
+import persistence.ConnectionPool;
+
+public class AdminController {
+
+    public void addRoutes(Javalin app, ConnectionPool connectionPool){
+    }
+
+}
---
+++ src/main/java/controllers/ClientController.java
@@ -0,0 +1,11 @@
+package controllers;
+
+import io.javalin.Javalin;
+import persistence.ConnectionPool;
+
+public class ClientController {
+
+    public void addRoutes(Javalin app, ConnectionPool connectionPool){
+    }
+
+}
