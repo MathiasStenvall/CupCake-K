9c3819a1cb4289aec6c0a6fe94f8be4d2432e7dd
ChristianKenter <cph-cn445@stud.ek.dk>
2026-04-08 12:54:31 +0200

    1).Created a config package.
    2). Created a SessionConfig class (And added sessionConfig-method)
    
    3). Created a ThymeleafConfig class (And added templateEngine-method).
    
    4). Created a ConnectionPool class (And added getInstance-method, getConnection-method and createHikariConnectionPool-method).
    
    5). Created an UML-Diagram in resources-package (And created UserMapper class, UserController class, Customer class, Administrator class and entites-packags)

---
+++ src/main/java/Main.java
@@ -0,0 +1,26 @@
+import config.SessionConfig;
+import config.ThymeleafConfig;
+import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinThymeleaf;
+import persistence.ConnectionPool;
+
+public class Main {
+
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "postgres";
+    private static final String URL = "jdbc:postgresql://localhost:5432/%s?currentSchema=public";
+    private static final String DB = "coffiime";
+    private static final ConnectionPool connectionPool = ConnectionPool.getInstance(USER, PASSWORD, URL, DB);
+
+    public static void main(String[] args) {
+
+        Javalin app = Javalin.create(config -> {
+            config.staticFiles.add("/public");
+            config.jetty.modifyServletContextHandler(handler -> handler.setSessionHandler(SessionConfig.sessionConfig()));
+            config.fileRenderer(new JavalinThymeleaf(ThymeleafConfig.templateEngine()));
+        }).start(7070);
+
+        app.get("/", ctx -> ctx.render("Index.html"));
+
+    }
+}
